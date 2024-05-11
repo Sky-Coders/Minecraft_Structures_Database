eb32dcf72e21727d298d4d6cb5e3562cdf04cb32
alexisuaguilaru <alexis.uaguilaru@gmail.com>
2024-05-10 21:12:44 -0600

    Code and Data for DIMENSIONS

---
+++ Data/WebScraping/jsonDimensions.py
@@ -0,0 +1,11 @@
+import pandas as pd
+from GenericsFunctions import dataframe_json
+
+def jsonDimensions():
+    dataDimensions = [('overworld',0,'Overworld'),('the_nether',-1,'Nether'),('the_end',1,'The End')]
+    dataColumnsDimension = ['identifier','numeric id','name']
+    dataframeDimensions = pd.DataFrame(data=dataDimensions, columns=dataColumnsDimension)
+    dataframe_json([dataframeDimensions],['jsonDimensions'])
+
+if __name__=='__main__':
+    jsonDimensions()
