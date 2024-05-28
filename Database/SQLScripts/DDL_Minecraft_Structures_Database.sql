2851d4d83b720e04a90137ecd2b95c8ac13026c4
alexisuaguilaru <alexis.uaguilaru@gmail.com>
2024-05-28 10:37:45 -0600

    ADD SQL file for DDL

---
+++ Database/SQLScripts/DDL_Minecraft_Structures_Database.sql
@@ -0,0 +1,8 @@
+-- Minecraft Structures Database 
+-- Descripción: Archivo SQL para la creación de la base de datos y DDL 
+-- Autores:
+
+-- DDL: Creación de la base de datos
+DROP DATABASE IF EXISTS Minecraft_Structures_Database;
+CREATE DATABASE Minecraft_Structures_Database:
+USE Minecraft_Structures_Database;
